f9fe93da986ffa6d10e7be84a9aaef3e04cfa681
Ivan Savov <ivan.savov@gmail.com>
2021-12-04 10:48:52 -0500

    fabfile.py requirements.txt

--- fabfile.py
+++ fabfile.py
@@ -31,26 +31,26 @@ def workon(repo, branch, vers=None):    # ??? 'vers/math' or just 'math' ???
     pass
 
 @task
-def save(repo):
+def save(repo='.'):
     """
     Save changes done on the working directory to the local repository.
     """
     with lcd(repo):
-        lines = local('git status --porcelain',capture=True).split("\n")
+        lines = local('git status --porcelain', capture=True).split("\n")
         commit_msg = ""
         for change in lines:
             if change:
-              (changetype,filename) = change.strip().split(" ", 1)
-              filename = filename.strip()
-              if changetype == 'M':
-                  prompt_text = "Include modifications to '%s'?" % (filename,)
-              else:
-                  prompt_text = "Include new file '%s'?" % (filename,)
-              response = prompt(prompt_text)
-              response = response.lower()
-              if len(response) > 0 and (response[0] == 'y' or response[0] == 't'):
-                  local('git add %s' % (filename,))
-                  commit_msg = "%s %s" % (commit_msg, filename)
+                changetype, filename = change.strip().split(" ", 1)
+                filename = filename.strip()
+                if changetype == 'M':
+                    prompt_text = "Include modifications to '%s'?" % filename
+                else:
+                    prompt_text = "Include new file '%s'?" % filename
+                response = prompt(prompt_text)
+                response = response.lower()
+                if len(response) > 0 and response[0] in ['y', 't']:
+                    local('git add %s' % filename)
+                    commit_msg = "%s %s" % (commit_msg, filename)
         if commit_msg:
             local('git commit -m "%s"' % (commit_msg,))
 
